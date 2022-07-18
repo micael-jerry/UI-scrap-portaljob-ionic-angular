820dfda5eaf1ba444bb3ea6f871fc1d3f950bd9d
Micael Jerry <hei.jerry.2@gmail.com>
2022-07-18 10:02:42 +0300

    update UI

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,13 +1,13 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  constructor(public http: HttpClient)  { }
+  constructor(public http: HttpClient) {}
 
-  getJobs(page){
-    return this.http.get('http://localhost:5000/api-portaljob?page='+page);
+  getJobs(page) {
+    return this.http.get('http://localhost:5000/api-portaljob?page=' + page);
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {ApiService} from "../api.service";
+import {Component} from '@angular/core';
+import {ApiService} from '../api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +7,28 @@ import {ApiService} from "../api.service";
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  public page: any;
+  public page: any = 1;
   public jobsData: any;
-  constructor(public api: ApiService) {}
 
-  search(){
+  constructor(public api: ApiService) {
     this.api.getJobs(this.page).subscribe(result => {
-      this.jobsData= result;
+      this.jobsData = result;
     });
+  }
+
+  next() {
+    this.api.getJobs(this.page + 1).subscribe(result => {
+      this.jobsData = result;
+      this.page += 1;
+    });
+  }
+
+  previous() {
+    if (this.page - 1 > 0) {
+      this.api.getJobs(this.page - 1).subscribe(result => {
+        this.jobsData = result;
+        this.page -= 1;
+      });
+    }
   }
 }
